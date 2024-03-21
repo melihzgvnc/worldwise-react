a0cde68aa4b42f8fd44ed65738ca725d678082bb
melih <melihzgvnc@gmail.com>
2024-03-21 18:41:14 +0000

    current city feature & backbutton component added

--- src/App.jsx
+++ src/App.jsx
@@ -11,30 +11,14 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
+import { CitiesProvider } from "./contexts/CitiesContext";
 
-const BASE_URL = "http://localhost:9000";
 
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch {
-        alert("There was an error loading data...");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
 
   return (
+    <CitiesProvider>
       <BrowserRouter>
         <Routes>
           <Route index element={<Homepage/>} />
@@ -45,21 +29,22 @@ function App() {
             <Route 
               index 
               element={<Navigate replace to="cities" />}
-            />
+              />
             <Route 
               path="cities" 
-              element={<CityList cities={cities} isLoading={isLoading}/>}
-            />
+              element={<CityList />}
+              />
             <Route 
               path="countries" 
-              element={<CountryList cities={cities} isLoading={isLoading} />}
-            />
+              element={<CountryList />}
+              />
             <Route path="cities/:id" element={<City />} />
             <Route path="form" element={<Form />}/>
           </Route>
           <Route path="*" element={<PageNotFound/>}></Route>
         </Routes>
       </BrowserRouter>
+    </CitiesProvider>
   )
 }
 
